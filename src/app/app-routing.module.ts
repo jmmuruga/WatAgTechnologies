49e65f28818b23d23fa0d657035e3eb77e2631b3
Arunprasanthm7 <arunprasanthm7@gmail.com>
2024-08-11 16:29:50 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesAndTechnologyComponent } from './services-and-technology/services-and-technology.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutComponent },
+  { path: 'servicesandtechnology', component: ServicesAndTechnologyComponent },
+  { path: 'contact', component: ContactComponent },
+  
 ];
 
 @NgModule({
