b5fa7c92bdca38ddaa6eb3f35be46bda309396e4
Arunprasanthm7 <arunprasanthm7@gmail.com>
2024-08-11 12:38:36 +0530

    header style changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,18 +5,22 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
-
-
   @ViewChild('header') myElement!: ElementRef;
-
   @HostListener('window:scroll', [])
+  
   onScroll(): void {
     if (window.scrollY > 10) {
       this.myElement.nativeElement.classList.add('headerSize');
+      this.logo=this.alternateLogo;
+      
     } else {
       this.myElement.nativeElement.classList.remove('headerSize');
+      this.logo='../../assets/Logo/logo.png';
     }
   }
+logo='../../assets/Logo/logo.png'
+alternateLogo='../../assets/Logo/logogreensolutionsindia-1-e1658147079576.png'
 
 }
