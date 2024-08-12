5e2739d2a6a1445009f49bee7d69be10ff27cd73
arunprasanthm7 <arunprasanthm7@gmail.com>
2024-08-12 13:05:41 +0530

    iot page designs are implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesAndTechnologyComponent } from './services-and-technology/services-and-technology.component';
 import { ContactComponent } from './contact/contact.component';
+import { IotComponent } from './iot/iot.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'aboutus', component: AboutComponent },
   { path: 'servicesandtechnology', component: ServicesAndTechnologyComponent },
   { path: 'contact', component: ContactComponent },
-  
+  { path: 'iot', component: IotComponent },
 ];
 
 @NgModule({
