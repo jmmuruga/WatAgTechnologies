99e15f373531d568de2a7cba917577e1ca9f940b
arunprasanthm7 <arunprasanthm7@gmail.com>
2024-08-13 11:10:09 +0530

    changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,16 +11,16 @@ export class HeaderComponent {
   @HostListener('window:scroll', [])
   
   onScroll(): void {
-    if (window.scrollY > 10) {
+    if (window.scrollY > 0) {
       this.myElement.nativeElement.classList.add('headerSize');
-      this.logo=this.alternateLogo;
+      // this.logo=this.alternateLogo;
       
     } else {
       this.myElement.nativeElement.classList.remove('headerSize');
-      this.logo='../../assets/Logo/logo.png';
+      // this.logo='../../assets/Logo/logo.png';
     }
   }
-logo='../../assets/Logo/logo.png'
-alternateLogo='../../assets/Logo/logogreensolutionsindia-1-e1658147079576.png'
+// logo='../../assets/Logo/logo.png'
+// alternateLogo='../../assets/Logo/logogreensolutionsindia-1-e1658147079576.png'
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  @ViewChild('header') myElement!: ElementRef;
+  @HostListener('window:scroll', [])
+  
+  onScroll(): void {
+    if (window.scrollY > 0) {
+      this.myElement.nativeElement.classList.add('headerSize');
+      // this.logo=this.alternateLogo;
+      
+    } else {
+      this.myElement.nativeElement.classList.remove('headerSize');
+      // this.logo='../../assets/Logo/logo.png';
+    }
+  }
+// logo='../../assets/Logo/logo.png'
+// alternateLogo='../../assets/Logo/logogreensolutionsindia-1-e1658147079576.png'
 }
