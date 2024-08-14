8aa3072db8ab7ddcde5edb34e3f175494945f783
arunprasanthm7 <arunprasanthm7@gmail.com>
2024-08-14 15:08:28 +0530

    changes

--- src/app/services-and-technology/services-and-technology.component.ts
+++ src/app/services-and-technology/services-and-technology.component.ts
@@ -10,12 +10,10 @@ export class ServicesAndTechnologyComponent {
 serviceImages1=['../../assets/Icons/icon-1-400x400.png','../../assets/Icons/icon-2-400x400.png','../../assets/Icons/icon-3-400x400.png','../../assets/Icons/icon-4w-400x400.png']
 serviceImages2=['../../assets/Icons/icon-1b.png','../../assets/Icons/icon-2b.png','../../assets/Icons/icon-3b.png','../../assets/Icons/icon-4b.png']
 ServicesOver(element:string,src:number){
-  debugger;
   const img = document.getElementById(element) as HTMLImageElement;
   img.src = this.serviceImages1[src];
 }
 ServicesOut(element:string,src:number){
-  debugger;
   const img = document.getElementById(element) as HTMLImageElement;
   img.src = this.serviceImages2[src];
 }
