ffadd2d953dfce911e1ded9b1c1d83d76821214c
jmmuruga <jmmuruga@gmail.com>
2024-10-10 09:55:18 +0530

    changes

--- src/app/services-and-technology/annual-maintainance/annual-maintainance.component.ts
+++ src/app/services-and-technology/annual-maintainance/annual-maintainance.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-annual-maintainance',
   templateUrl: './annual-maintainance.component.html',
   styleUrls: ['./annual-maintainance.component.css']
 })
 export class AnnualMaintainanceComponent {
+  fullName : string = '';
+
+
   scrollToTableTop(scrlId: any) {
     const element = document.getElementById(scrlId);
     if (element) {
@@ -15,4 +19,38 @@ export class AnnualMaintainanceComponent {
       });
     }
 }
+messageForm = new FormGroup({
+  to_name: new FormControl('Sir/Madam'),
+  FirstName: new FormControl('', [Validators.required]),
+  LastName: new FormControl('', [Validators.required]),
+  CompanyName: new FormControl(''),
+  PhoneNumber: new FormControl('', [Validators.required]),
+  EmailId: new FormControl('', [Validators.email,Validators.required]),
+  Message: new FormControl('', [Validators.required]),
+});
+async send() {
+  this.fullName = this.messageForm.value?.FirstName || '' + this.messageForm.value?.LastName || '';
+  emailjs.init('csN6k99_P17n1XR6h');
+  let response = await emailjs.send("service_yo6s73m","template_7cx6ryi",{
+    from_name:this.fullName,
+    to_name: "WatAg Technologies",
+    mailId: this.messageForm.value.EmailId,
+    phone_number:  this.messageForm.value.PhoneNumber,
+    message: this.messageForm.value.Message,
+    });
+  if (response.status == 200 && response.text == 'OK') {
+    this.messageForm.reset();
+  }
+}
+
+async onSubmit() {
+  debugger
+  if (this.messageForm.valid) {
+    await this.send();
+    alert('form Submitted');
+  } else {
+    this.messageForm.markAllAsTouched();
+  }
+}
+
 }
--- src/app/services-and-technology/services-and-technology.component.ts
+++ src/app/services-and-technology/services-and-technology.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-services-and-technology',
   templateUrl: './services-and-technology.component.html',
   styleUrls: ['./services-and-technology.component.css']
 })
 export class ServicesAndTechnologyComponent {
-
+  fullName : string = '';
 serviceImages1=['../../assets/Icons/icon-1-400x400.png','../../assets/Icons/icon-2-400x400.png','../../assets/Icons/icon-3-400x400.png','../../assets/Icons/icon-4w-400x400.png','../../assets/Icons/icon-4w-400x400.png','../../assets/Icons/icon-4w-400x400.png']
 serviceImages2=['../../assets/Icons/icon-1b.png','../../assets/Icons/icon-2b.png','../../assets/Icons/icon-3b.png','../../assets/Icons/icon-4b.png','../../assets/Icons/icon-4b.png','../../assets/Icons/icon-4b.png']
 ServicesOver(element:string,src:number){
@@ -35,5 +36,41 @@ services = [
   {id:5,src:'../../assets/Icons/environmental-removebg-preview.png',name:'Environmental Consulting Service', link:'/servicesandtechnology/environmentalconsultingservice'},
   {id:6,src:'../../assets/Icons/serviceMaintainance-removebg-preview.png',name:'Annual Maintainance', link:'/servicesandtechnology/annualmaintainancecomponent'},
 ]
+
+messageForm = new FormGroup({
+  to_name: new FormControl('Sir/Madam'),
+  FirstName: new FormControl('', [Validators.required]),
+  LastName: new FormControl('', [Validators.required]),
+  CompanyName: new FormControl(''),
+  PhoneNumber: new FormControl('', [Validators.required]),
+  EmailId: new FormControl('', [Validators.email,Validators.required]),
+  Message: new FormControl('', [Validators.required]),
+});
+
+
+async send() {
+  this.fullName = this.messageForm.value?.FirstName || '' + this.messageForm.value?.LastName || '';
+  emailjs.init('csN6k99_P17n1XR6h');
+  let response = await emailjs.send("service_yo6s73m","template_7cx6ryi",{
+    from_name:this.fullName,
+    to_name: "WatAg Technologies",
+    mailId: this.messageForm.value.EmailId,
+    phone_number:  this.messageForm.value.PhoneNumber,
+    message: this.messageForm.value.Message,
+    });
+  if (response.status == 200 && response.text == 'OK') {
+    this.messageForm.reset();
+  }
+}
+
+async onSubmit() {
+  debugger
+  if (this.messageForm.valid) {
+    await this.send();
+    alert('form Submitted');
+  } else {
+    this.messageForm.markAllAsTouched();
+  }
+}
 
 }
